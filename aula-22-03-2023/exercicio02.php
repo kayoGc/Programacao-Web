d033cf122fb7d52c3474057d1721799515b8a381
Kayo Victor G. Costa <kayivicti@gmail.com>
2023-03-26 17:50:33 -0300

    exercicios 22-03-23

--- aula-22-03-2023/exercicio02.php
+++ aula-22-03-2023/exercicio02.php
@@ -1,30 +1,28 @@
 <?php 
-	$horaTrabalhada = 7;
-	$salarioHora = 8.50;
-	$numeroDependentes = 1; 
+	$horaTrabalhada = 60;
+	$salarioHora = 10;
+	$numeroDependentes = 0; 
 
 	$salarioBruto = $horaTrabalhada * $salarioHora + (50 * $numeroDependentes); 
 	if ($salarioBruto <= 1000) {
-		$INSS = $salarioBruto * 8.5/100;
+		$INSS = $salarioBruto * (8.5 / 100); 	
 	}else { 
-
-		$INSS = $salarioBruto * 9/100;
-
+		$INSS = $salarioBruto * (9 / 100);
 	} 
 
 	if ($salarioBruto <= 500) {
 		$IR = 0;
 			
 	}elseif ($salarioBruto > 500 && $salarioBruto <= 1000) {
-		$IR = $salarioBruto * 5/100; 
+		$IR = $salarioBruto * (5 / 100); 
 
 	}else { 
-		$IR = $salarioBruto * 7/100;
+		$IR = $salarioBruto * (7 / 100);
 
 	} 
 
 	$salarioLiquido = $salarioBruto - $INSS - $IR;
-	echo "$salarioBruto <br> $INSS <br> $IR <br> $salarioLiquido";
+	echo "Salario Bruto: R$$salarioBruto <br>Desconto INSS: R$$INSS <br>Desconto IR: R$$IR <br>Salario liquido: R$$salarioLiquido";
 
 
  ?>
