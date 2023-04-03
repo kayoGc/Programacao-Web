78dd1dbe09f86cb8125e40ed5df7f2f970ab030c
kayoGc <kayivicti@gmail.com>
2023-04-03 19:36:32 -0300

    Aula 03-04-2023

---
+++ aula-03-04-2023/calculadora.php
@@ -0,0 +1,54 @@
+<?php 
+	function fSomar($num1, $num2) {
+		$soma = $num1 + $num2;
+		return $soma;
+	}
+
+	function fSubtrair($num1, $num2) {
+		$subtracao = $num1 - $num2;
+		return $subtracao;
+	}
+
+	function fMultiplicar($num1, $num2) {
+		$multiplicacao = $num1 * $num2;
+		return $multiplicacao;
+	}
+
+	function fDividir($num1, $num2) {
+		$divisao = 0;
+		if ($num2 == 0) {
+			echo "Impossivel dividir por 0 <br/>";
+		} else if ($num1 == 0){
+			echo "Impossivel dividir 0 <br/>";
+		}
+		else {
+			$divisao = $num1 / $num2;	
+		}
+		return $divisao;  
+	}
+
+	$num1 = 50;
+	$num2 = 10;
+	$operador = "/";
+
+	switch ($operador) {
+		case '+':
+			$resultado = fsomar($num1, $num2);
+			break;
+		case "-":
+			$resultado = fSubtrair($num1, $num2);
+			break;
+		case '*':
+			$resultado = fMultiplicar($num1, $num2);
+			break;
+		case "/":
+			$resultado = fDividir($num1, $num2);
+			break;
+		default:
+			echo "Operador inválido";
+			break;
+	}
+
+	print("$num1 $operador $num2 = ");
+	printf("%.2f", $resultado);
+?>
