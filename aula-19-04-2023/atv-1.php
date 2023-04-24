6cc24d67d5ecf9025459a31c99b9f15c67a02a43
kayoGc <kayivicti@gmail.com>
2023-04-24 12:53:14 -0300

    atv3 com sizeof

--- aula-19-04-2023/atv-1.php
+++ aula-19-04-2023/atv-1.php
@@ -3,10 +3,10 @@
 	$notas = array("Maria" => 4, "Mario" => 6, "Mauro" => 7, "Maura" => 9, "Matias" => 6, "Matios" => 8.5, "Manuel" => 7, "Manuela" => 3, "Matuê" => 5, "Matuâ" => 8);
 	$somaNotas = 0;
 	$maiorNota = 0;
-	$cont = 0;
+	#$cont = 0;
 	while ($i = key($notas)) {
 		$somaNotas += $notas[$i];
-		$cont++;
+		#$cont++;
 		if ($maiorNota == 0) {
 			$maiorNota = $i;
 		} elseif ($notas[$maiorNota] < $notas[$i]) {
@@ -14,7 +14,7 @@
 		}
 		next($notas);
 	}
-	$media = $somaNotas / $cont;
+	$media = $somaNotas / sizeof($notas);
 
 	echo "A média da sala é: $media. E a maior nota foi do aluno(a) $maiorNota com a nota $notas[$maiorNota]";
  ?>              
