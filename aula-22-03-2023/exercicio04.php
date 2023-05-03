bbf2ee17216e3d4c27c1203bcbad5de2de8039e5
_Kayovictorr <117952963+kayoGc@users.noreply.github.com>
2023-05-03 18:58:26 -0300

    Delete aula-22-03-2023 directory

--- aula-22-03-2023/exercicio04.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	$valor = 300; 
-	$desconto = 0.09;
-	$valorFinal = $valor + $valor * $desconto; 
-
-	echo "O valor do produto com desconto é: R$$valorFinal";
-
- ?>
--- aula-22-03-2023/exercicio07.php
+++
@@ -1,8 +0,0 @@
-<?php 	
-		$tempo = 50;
-		$velocidade = 20;
-		$distancia = $tempo * $velocidade;
-		$litrosUsados = $distancia / 12;
-
-		printf("Foram usados %.2f litros para percorrer %.2fkm", $litrosUsados, $distancia);
- ?>
