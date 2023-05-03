aba3fcc2e44e500cb41d423c0ad510096228b5a2
_Kayovictorr <117952963+kayoGc@users.noreply.github.com>
2023-05-03 18:58:37 -0300

    Delete aula-19-04-2023 directory

--- aula-19-04-2023/atv-3.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$numeros = array(1, 8, -1, 9, 3, 18, 91, 1.7, -10, 19);
-	$multiplicador = 5;
-
-	foreach ($numeros as $numero) {
-		echo "$numero x $multiplicador = " . $numero * $multiplicador . "<br>";
-	}
- ?>
--- aula-19-04-2023/estrutura2-array.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$nomes = array("MA" => "Maria", "ZE" => "José", "PE" => "Pedro", "AN" => "Ana");
-	while($chave = key($nomes)) {
-		
-
-		echo "<br/> " . $chave;
-		next($nomes);
-	}
- ?>
